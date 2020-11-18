b5a491f6665f3dac51ae2ecb9ffc522a65b9339d
JayPatel1314 <69890535+JayPatel1314@users.noreply.github.com>
2020-11-18 20:01:48 +0530

    Update animequote_string.py

--- SaitamaRobot/modules/animequote_string.py
+++ SaitamaRobot/modules/animequote_string.py
@@ -108,6 +108,6 @@ ANIMEQUOTE = (
     "The same melody repeats itself as the song grows richer and more beautiful. Wouldn’t it be wonderful if we could live seemingly constant lives while experiencing changes bit by bit? – Sayuri Kurata (Kanon)",
     "I never thought about winning making me cool, or losing making me uncool. But you’re right. If anything… Giving up before you even try is kind of uncool. That’s why I want to be someone who’ll fight as many times as it takes, and never give up. That’s got to be way more cooler. – Tachibana Hotaru (Aoharu x Kikanjuu)",
     "Stand tall and be proud. No matter how weak or unworthy you feel keep your heart burning, grit your teeth and move forward. If you just curl up in a ball and hide, time will pass you by. It won't stop for you while you wallow in your grief. - Kyojuro Rengoku (Demon Slayer:Kimetsu No Yaiba)",
-    "You think you can just escape? Walk out of this world like it’s some bad movie? It’s too easy to make like a bird. To flap your wings as you chicken out after causing all these problems. The tough part’s walking. Taking what comes along and walking right through it. That’s what real people do. – Eikichi Onizuka (Great Teacher Onizuka:GTO)
+    "You think you can just escape? Walk out of this world like it’s some bad movie? It’s too easy to make like a bird. To flap your wings as you chicken out after causing all these problems. The tough part’s walking. Taking what comes along and walking right through it. That’s what real people do. – Eikichi Onizuka (Great Teacher Onizuka:GTO)",
 )
 
