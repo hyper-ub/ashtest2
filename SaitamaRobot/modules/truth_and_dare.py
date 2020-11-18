677ef68fd9cded451655c9f7cc4f97e8d062762c
Uday0011 <73984830+Uday0011@users.noreply.github.com>
2020-11-18 15:49:11 +0530

    Update truth_and_dare.py

--- SaitamaRobot/modules/truth_and_dare.py
+++ SaitamaRobot/modules/truth_and_dare.py
@@ -4,7 +4,7 @@ import SaitamaRobot.modules.truth_and_dare_string as truth_and_dare_string
 from SaitamaRobot import dispatcher
 from telegram import ParseMode, Update, Bot
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from telegram.ext import run_async
+from telegram.ext import CommandHandler, run_async, Filters
 
 
 @run_async
