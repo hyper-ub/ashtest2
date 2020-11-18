918095ee592807891feff64377d12de9e1a75f53
Uday0011 <73984830+Uday0011@users.noreply.github.com>
2020-11-18 17:03:48 +0530

    Update animequote.py

--- SaitamaRobot/modules/animequote.py
+++ SaitamaRobot/modules/animequote.py
@@ -9,7 +9,7 @@ from telegram.ext import CallbackContext, run_async
 @run_async
 def animequote(update: Update, context: CallbackContext):
     args = context.args
-    update.effective_message.reply_text(random.choice(animequotestring.ANIMEQUOTE))
+    update.effective_message.reply_text(random.choice(animequote_string.ANIMEQUOTE))
 
 __help__ = """
  • `/animequote`*:* for random animequote
