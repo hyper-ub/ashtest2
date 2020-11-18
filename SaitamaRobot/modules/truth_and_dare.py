41571d66ac1bfc495ecdbcbc83e8f43ff489c795
Uday0011 <73984830+Uday0011@users.noreply.github.com>
2020-11-18 15:46:54 +0530

    Update truth_and_dare.py

--- SaitamaRobot/modules/truth_and_dare.py
+++ SaitamaRobot/modules/truth_and_dare.py
@@ -9,12 +9,12 @@ from telegram.ext import run_async
 
 @run_async
 def truth(bot: Bot, update: Update):
-    args = context.args
+    context = context.args
     update.effective_message.reply_text(random.choice(truth_and_dare_string.TRUTH))
 
 @run_async
 def dare(bot: Bot, update: Update):
-    args = context.args
+    context = context.args
     update.effective_message.reply_text(random.choice(truth_and_dare_string.DARE))
 
 __help__ = """
