7c8c0239916391388e64847ff82f5462623cdec1
JayPatel1314 <69890535+JayPatel1314@users.noreply.github.com>
2020-11-18 17:02:58 +0530

    Update animequote_string.py

--- SaitamaRobot/modules/animequote_string.py
+++ SaitamaRobot/modules/animequote_string.py
@@ -1,5 +1,6 @@
 ANIMEQUOTE = (
     "There are days when nothing goes right. There are days when you stumble and fall. There are days when you just want to cry. To cry a lot. To sleep a lot. Or even eat a lot. It’s alright, as long as you pick yourself up again. – Yuki Takeya (Gakkougurashi!)",
     "Living has it own challenges. I’ll give you just one piece of advice. Don’t quit. Yes, when you die, you die. But if you give up, you’re definitely going to die. That, I am sure of. – Brittany (Hai to Gensou no Grimgar)",
+    "All I Can Do Is Work Hard! That’s The Story Of My Life! - Tanjiro Kamado (Demon Slayer: Kimetsu no yaiba)"
 )
 
