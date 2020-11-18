743e05371bf1f9907aabfd5cd57087250977b9d3
JayPatel1314 <69890535+JayPatel1314@users.noreply.github.com>
2020-11-18 17:06:57 +0530

    Update animequote_string.py

--- SaitamaRobot/modules/animequote_string.py
+++ SaitamaRobot/modules/animequote_string.py
@@ -1,6 +1,13 @@
 ANIMEQUOTE = (
     "There are days when nothing goes right. There are days when you stumble and fall. There are days when you just want to cry. To cry a lot. To sleep a lot. Or even eat a lot. It’s alright, as long as you pick yourself up again. – Yuki Takeya (Gakkougurashi!)",
     "Living has it own challenges. I’ll give you just one piece of advice. Don’t quit. Yes, when you die, you die. But if you give up, you’re definitely going to die. That, I am sure of. – Brittany (Hai to Gensou no Grimgar)",
-    "All I Can Do Is Work Hard! That’s The Story Of My Life! - Tanjiro Kamado (Demon Slayer: Kimetsu no yaiba)"
+    "All I Can Do Is Work Hard! That’s The Story Of My Life! - Tanjiro Kamado (Demon Slayer: Kimetsu no yaiba)",
+    "You can’t sit around envying other people’s worlds. You have to go tout and change your own. – Shinichi Chiaki (Nodame Cantabile)",
+    "It’s not like something is going to happen because I believe in it. Sometimes you end up worse off because you believed in something too much. There are times it’s tough to believe in something. – Eureka (Eureka Seven)",
+    " I don’t think I’ll learn anything if I don’t try to do something about it on my own. – Chizuru Hishiro (ReLIFE)",
+    " A dropout will beat a genius through hard work. – Rock Lee (Naruto)",
+    "You guys sure are a worthless bunch. If you wanna wallow in your tears, save it for after you’ve done everything you can. – Seiya Kanie (Amagi Brilliant Park)",
+    "Smile. Not for anyone else, but for yourself. Show yourself your own smile. You’ll feel better then. – Rintaro Tatewaki (Magic-Kyun! Renaissance)",
+    "It may be hard right now, but you must silence those thoughts. Stop counting only those things that you have lost. What is gone, is gone. – Jinbei (One Piece)",
 )
 
