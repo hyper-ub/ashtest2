124923a08906b46636924148fe0d050783648db3
Uday0011 <73984830+Uday0011@users.noreply.github.com>
2020-11-18 16:57:43 +0530

    Update animequote.py

--- SaitamaRobot/modules/animequote.py
+++ SaitamaRobot/modules/animequote.py
@@ -1,1 +1,22 @@
-z
+import html
+import random
+import SaitamaRobot.modules.animequotestring as animequotestring
+from SaitamaRobot import dispatcher
+from telegram import ParseMode, Update, Bot
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from telegram.ext import CallbackContext, run_async
+
+@run_async
+def animequote(update: Update, context: CallbackContext):
+    args = context.args
+    update.effective_message.reply_text(random.choice(animequotestring.ANIMEQUOTE))
+
+__help__ = """
+ • `/animequote`*:* for random animequote
+"""
+
+ANIMEQUOTE_HANDLER = DisableAbleCommandHandler("animequote", animequote)
+
+dispatcher.add_handler(ANIMEQUOTE_HANDLER)
+
+__mod_name__ = "Animequote"
