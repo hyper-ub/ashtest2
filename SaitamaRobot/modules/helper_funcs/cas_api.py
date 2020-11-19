5dea8bd99e2bdad6de6abea0b9109b5c6e7a22bc
Uday0011 <73984830+Uday0011@users.noreply.github.com>
2020-11-19 08:45:18 +0530

    Update cas_api.py

--- SaitamaRobot/modules/helper_funcs/cas_api.py
+++ SaitamaRobot/modules/helper_funcs/cas_api.py
@@ -3,7 +3,7 @@ CAS_QUERY_URL = "https://api.cas.chat/check?user_id="
 DL_DIR = "./csvExports"
 
 def get_user_data(user_id):
-    with requests.request('GET', CAS_QUERY_URL + str(user_id)) as userdata_raw:
+    with request('GET', CAS_QUERY_URL + str(user_id)) as userdata_raw:
         userdata = json.loads(userdata_raw.text)
         return userdata
 
