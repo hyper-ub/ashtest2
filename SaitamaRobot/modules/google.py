62c8b79025b0d5a62e2521651b47924a529e9f34
JayPatel1314 <69890535+JayPatel1314@users.noreply.github.com>
2020-11-23 18:06:27 +0530

    Update google.py

--- SaitamaRobot/modules/google.py
+++ SaitamaRobot/modules/google.py
@@ -4,10 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 from SaitamaRobot.events import register
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, run_async
+from SaitamaRobot import client
 import sys
 import shutil
 from re import findall
