fa673f86e26dd55b505642d75f88498ba9e6a328
JayPatel1314 <69890535+JayPatel1314@users.noreply.github.com>
2020-11-23 17:42:26 +0530

    Update google.py

--- SaitamaRobot/modules/google.py
+++ SaitamaRobot/modules/google.py
@@ -14,7 +14,7 @@ from telethon.tl import types
 from telethon.tl.types import *
 import html2text
 
-
+@run_async
 async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
 
