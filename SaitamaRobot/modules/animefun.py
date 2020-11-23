ebeb098991c0d5aabd0d7b2c424edf9f039575a6
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-23 19:26:32 +0530

    Update animefun.py

--- SaitamaRobot/modules/animefun.py
+++ SaitamaRobot/modules/animefun.py
@@ -3,6 +3,7 @@ from PIL import Image
 import requests
 import os
 import random
+import nekos
 from SaitamaRobot import dispatcher
 from telegram import ParseMode, Update, Bot
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
@@ -25,8 +26,17 @@ def fact(update: Update, context: CallbackContext):
     msg.reply_text(fact)
 
 
+@run_async
+def neko(update, context):
+    msg = update.effective_message
+    animefun = "neko"
+    msg.reply_photo(nekos.img(animefun))
+
+
 WHY_HANDLER = DisableAbleCommandHandler("why", why)
 FACT_HANDLER = DisableAbleCommandHandler("fact", fact)
+NEKO_HANDLER = DisableAbleCommandHandler("neko", neko)
 
 dispatcher.add_handler(WHY_HANDLER)
 dispatcher.add_handler(FACT_HANDLER)
+dispatcher.add_handler(NEKO_HANDLER)
