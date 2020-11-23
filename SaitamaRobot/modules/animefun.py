1ba8af07d69a77bd9f51005898e2866b12b10e06
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-23 18:42:23 +0530

    Create animefun.py

---
+++ SaitamaRobot/modules/animefun.py
@@ -0,0 +1,18 @@
+import html
+import random
+from SaitamaRobot import dispatcher
+from telegram import ParseMode, Update, Bot
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from telegram.ext import CallbackContext, run_async
+
+
+@run_async
+def animewall(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    anime = requests.get("https://api.computerfreaker.cf/v1/anime").json()
+    url = anime.get("url")  
+    msg.reply_photo(url)
+
+ANIMEWALL_HANDLER = DisableAbleCommandHandler("animewall", animewall)
+
+dispatcher.add_handler(ANIMEWALL_HANDLER)
