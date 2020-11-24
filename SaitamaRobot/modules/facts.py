60ba0f2f5f7335d2fb245ab397165c0768e0cf67
JayPatel1314 <69890535+JayPatel1314@users.noreply.github.com>
2020-11-24 09:40:01 +0530

    Create facts.py

---
+++ SaitamaRobot/modules/facts.py
@@ -0,0 +1,18 @@
+import html
+import random
+from SaitamaRobot import dispatcher
+from telegram import ParseMode, Update, Bot
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from telegram.ext import CallbackContext, run_async
+
+
+@run_async
+def fact(update: Update, context: CallbackContext):
+    msg = update.effective_message
+    fact = requests.get("https://nekos.life/api/v2/fact").json()
+    fact = fact.get("fact")  
+    msg.reply_text(fact)
+
+FACT_HANDLER = DisableAbleCommandHandler("fact", fact)
+
+dispatcher.add_handler (FACT_HANDLER)
