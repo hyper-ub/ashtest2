b3143d27ab96caa1e4b19576e71862d4782ff669
IronMan <73984830+Uday0011@users.noreply.github.com>
2020-11-26 17:27:42 +0530

    Update animequote.py

--- SaitamaRobot/modules/animequote.py
+++ SaitamaRobot/modules/animequote.py
@@ -10,7 +10,7 @@ from telegram.ext import CallbackContext, run_async
 @run_async
 def aq(update: Update, context: CallbackContext):
     args = context.args
-    update.effective_message.reply_text(❝random.choice(animequote_string.ANIMEQUOTE))
+    update.effective_message.reply_text(random.choice(animequote_string.ANIMEQUOTE))
 
 __help__ = """
  • `/aq`*:* for random animequote
